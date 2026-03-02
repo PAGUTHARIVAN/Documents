b044b841ee7b724d1897909453cdf12d27be8db2
pagutharivan <pagutharivan2004@gmail.com>
2026-03-02 21:45:13 +0530

    successfully created the first Hello world program

--- Pagu-Java-Projects/Main.java
+++ Pagu-Java-Projects/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Pagu welcome to the Java world (The king of Enterprise systems)");
+    void main() {
+        System.out.println("Hello, Pagu Welcome to the Java world");
     }
 }
